8763adec51cd26fef81d2defbee5be8b6eb16003
Chinmay <chinmay.kabi@live.com>
2021-05-24 01:21:38 +0530

    feat: Kep parsing error solved

--- server/init_db.js
+++ server/init_db.js
@@ -5,7 +5,7 @@ const params = {
   type: process.env.type,
   projectId: process.env.project_id,
   privateKeyId: process.env.private_key_id,
-  privateKey: process.env.private_key,//.replace(/\\n/g, '\n'),
+  privateKey: process.env.private_key.replace(/\\n/g, '\n'),
   clientEmail: process.env.client_email,
   clientId: process.env.client_id,
   authUri: process.env.auth_uri,
